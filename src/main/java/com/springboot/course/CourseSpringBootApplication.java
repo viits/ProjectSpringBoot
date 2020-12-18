c682a5354c3c1f0c0dba033cd2246dc4619a76c9
viits <toyobr@hotmail.com>
2020-12-18 10:38:12 -0300

    criação do projeto

---
+++ src/main/java/com/springboot/course/CourseSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSpringBootApplication.class, args);
+	}
+
+}
