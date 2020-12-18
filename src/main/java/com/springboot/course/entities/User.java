f823657daba92369bf44b79835c8e32916150c1d
viits <toyobr@hotmail.com>
2020-12-18 11:24:00 -0300

    Database h2,Proprio do java

--- src/main/java/com/springboot/course/entities/User.java
+++ src/main/java/com/springboot/course/entities/User.java
@@ -2,9 +2,18 @@ package com.springboot.course.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+	//GENERATEDVALUE = faz o Id ser auto increments
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
