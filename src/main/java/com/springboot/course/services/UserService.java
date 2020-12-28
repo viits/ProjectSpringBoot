c588cd8b56076b806f2061db65cbf85185530f1f
viits <toyobr@hotmail.com>
2020-12-28 09:43:14 -0300

    Exception Handle - findById

--- src/main/java/com/springboot/course/services/UserService.java
+++ src/main/java/com/springboot/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.springboot.course.entities.User;
 import com.springboot.course.repositories.UserRepository;
+import com.springboot.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> user = userRepository.findById(id);
-		return user.get();
+		return user.orElseThrow(()-> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User user) {
