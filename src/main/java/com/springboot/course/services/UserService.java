3d1f25b1dc63dfeeda08ca1c4665de522b823bc9
viits <toyobr@hotmail.com>
2020-12-28 09:19:35 -0300

    Update user

--- src/main/java/com/springboot/course/services/UserService.java
+++ src/main/java/com/springboot/course/services/UserService.java
@@ -32,4 +32,16 @@ public class UserService {
 		userRepository.deleteById(id);
 	}
 	
+	public User update(Long id,User user) {
+		User entity =  userRepository.getOne(id);
+		updateData(entity,user);
+		return userRepository.save(entity);
+	}
+	private void updateData(User entity,User user) {
+		entity.setName(user.getName());
+		entity.setEmail(user.getEmail());
+		entity.setPhone(user.getPhone());
+		
+	}
+	
 }
